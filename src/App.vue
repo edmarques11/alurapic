63215150def7c3def2bf119a18a79eb7d30a096e
Edmarques Lima <52663280+edmarques11@users.noreply.github.com>
2021-07-22 22:17:21 -0300

    iniciando projeto alurapic

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <h1>{{ titulo }}</h1>
+    <ul>
+      <li v-for="(foto, index) of fotos" :key="index">
+        <img :src="foto.url" :alt="foto.titulo" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      titulo: "Alura PIC",
+      fotos: [
+        {
+          url: "https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg",
+          titulo: "cachorro",
+        },
+        {
+          url: "https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg",
+          titulo: "cachorro",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
