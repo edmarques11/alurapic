53f40cdd7989ab06a4e730dc31e79966e0183a2d
Edmarques Lima <52663280+edmarques11@users.noreply.github.com>
2021-07-24 23:58:30 -0300

    integraçao com api e slots

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,73 @@
 <template>
-  <div>
-    <h1>{{ titulo }}</h1>
-    <ul>
-      <li v-for="(foto, index) of fotos" :key="index">
-        <img :src="foto.url" :alt="foto.titulo" />
-      </li>
-    </ul>
-  </div>
+    <div class="corpo">
+        <h1 class="centralizado">{{ titulo }}</h1>
+        <ul class="lista-fotos">
+            <li
+                class="lista-fotos-item"
+                v-for="(foto, index) of fotos"
+                :key="index"
+            >
+                <meu-painel :titulo="foto.titulo">
+                    <img
+                        :src="foto.url"
+                        :alt="foto.titulo"
+                        class="imagem-responsiva"
+                    />
+                </meu-painel>
+            </li>
+        </ul>
+    </div>
 </template>
 
 <script>
+import Painel from "./components/shared/painel/Painel.vue";
+
 export default {
-  data() {
-    return {
-      titulo: "Alura PIC",
-      fotos: [
-        {
-          url: "https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg",
-          titulo: "cachorro",
-        },
-        {
-          url: "https://matsudapet.com.br/blog/wp-content/uploads/2019/08/shutterstock_559799125-compressed.jpg",
-          titulo: "cachorro",
-        },
-      ],
-    };
-  },
+    name: 'App',
+
+    components: {
+        'meu-painel': Painel,
+    },
+
+    data() {
+        return {
+            titulo: "Alura PIC",
+            fotos: [],
+        };
+    },
+
+    created() {
+        this.$http
+            .get("http://localhost:3000/v1/fotos")
+            .then((response) => response.json())
+            .then(
+                (fotos) => (this.fotos = fotos),
+                (error) => console.log(error)
+            );
+    },
 };
 </script>
 
 <style>
+.centralizado {
+    text-align: center;
+}
+
+.corpo {
+    font-family: Helvetica, sans-serif;
+    margin: 0 auto;
+    width: 96%;
+}
+
+.lista-fotos {
+    list-style: none;
+}
+
+.lista-fotos .lista-fotos-item {
+    display: inline-block;
+}
+
+.imagem-responsiva {
+    width: 100%;
+}
 </style>
