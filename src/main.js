8dc6f57c09d24474cbcd89edfcc941e2fb167884
Edmarques Lima <52663280+edmarques11@users.noreply.github.com>
2021-08-24 21:29:43 -0300

    adicionando boostrap no projeto

--- src/main.js
+++ src/main.js
@@ -5,6 +5,9 @@ import VueRouter from 'vue-router';
 import { routes } from './routes';
 import './directives/Transform';
 import VeeValidate from 'vee-validate';
+import msg from './pt_BR';
+import 'bootstrap/dist/css/bootstrap.css';
+import './assets/css/teste.css'
 
 Vue.use(VueResource);
 Vue.http.options.root = 'http://localhost:3000/v1';
@@ -13,7 +16,14 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({ routes, mode: 'history' });
 
-Vue.use(VeeValidate);
+Vue.use(VeeValidate, {
+    locale: 'pt_BR',
+    dictionary: {
+        pt_BR: {
+            messages: msg
+        }
+    }
+});
 
 new Vue({
     el: '#app',
